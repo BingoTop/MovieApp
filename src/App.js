93710a8d443678b9513837b060e1da7aa17f2809
bingotop <wodnjs3600@naver.com>
2020-12-03 22:37:56 +0900

    2020/12/03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from "react";
+import Movie from "./components/Movie";
+
+require('dotenv').config();
+
+const API_KEY= process.env.REACT_APP_API_KEY;
+const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 function App() {
+  const [movies,setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(()=>{
+    getMovies(API_URL);
+  },[])
+
+  const getMovies = (API)=>{
+      fetch(API).then(res=>res.json())
+      .then(data=>{
+        setMovies(data.results);
+      });
+  }
+  const handleOnSubmit = (e)=>{
+    e.preventDefault();
+    if(searchTerm){
+      getMovies(SEARCH_API + searchTerm);
+      setSearchTerm('');
+    }
+  };
+
+  const handleOnChange = (e)=>{
+    setSearchTerm(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+        <input 
+        type="search" 
+        placeholder="search" 
+        className="search"
+        value={searchTerm}
+        onChange={handleOnChange}
+        />
+        </form>
       </header>
+    <div className="movie-container">
+      {movies.length > 0 && movies.map((movie)=>{
+        return <Movie key={movie.id} {...movie}/>
+      })}
     </div>
-  );
+    </>
+    );
 }
 
 export default App;
